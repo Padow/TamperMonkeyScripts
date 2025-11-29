8a1a2f015359def4f904ef3ad25cdc6c5c766110
Padow <patrick.douchement@gmail.com>
2025-11-29 21:18:23 +0100

    Create YoutubeAmbiantModeDisabler.js

---
+++ YoutubeAmbiantModeDisabler.js
@@ -0,0 +1,44 @@
+// ==UserScript==
+// @name         Youtube ambiant mode disabler
+// @namespace    http://tampermonkey.net/
+// @version      2025-11-29
+// @description  disable ambiant light
+// @copyright none
+// @require https://code.jquery.com/jquery-latest.min.js
+// @match https://www.youtube.com/*
+// ==/UserScript==
+
+/* globals jQuery, $, waitForKeyElements */
+
+$(document).ready(function() {
+
+    var k = 0;
+    var text = "Éclairage de cinéma"
+
+    function checkAmbientMode() {
+        console.log("count :" + k)
+        if (k > 500) {
+            console.log("clearInterval " + k)
+            window.clearInterval(itvl3);
+        } else if (/https:\/\/www\.youtube\.com\/watch.*/.test(document.URL)) {
+            $(".ytp-settings-button").click()
+            var ariaChecked = $('div:contains("'+text+'")').parents('div[class^="ytp-menuitem"]')[0].ariaChecked
+            if (ariaChecked == "true") {
+                $('div:contains("'+text+'")').parents('div[class^="ytp-menuitem"]')[0].click()
+                k = 501
+            }
+
+           if(ariaChecked == "false") {
+               k = 501
+           }
+            $(".ytp-settings-button").click()
+        }
+        k++
+    }
+
+    var itvl3 = window.setInterval(function() {
+        checkAmbientMode();
+    }, 1000);
+
+
+});
