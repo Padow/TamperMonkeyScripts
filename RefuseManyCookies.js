0f74f522f50cd00a436184142618432e7aea292a
Padow <patrick.douchement@gmail.com>
2026-03-04 12:12:59 +0100

    Update RefuseManyCookies.js

--- RefuseManyCookies.js
+++ RefuseManyCookies.js
@@ -11,22 +11,8 @@
 (function() {
     'use strict';
 
-    console.log("############################# SCRIPT LOADED ##########################################")
-    const excludeWebsites = []// ==UserScript==
-// @name         refuse cookie
-// @namespace    http://tampermonkey.net/
-// @version      2026-02-26
-// @description  try to take over the world!
-// @match        https://*/*
-// @icon https://raw.githubusercontent.com/Padow/TamperMonkeyScripts/refs/heads/main/NoCookies.png
-// @copyright none
-// ==/UserScript==
-
-(function() {
-    'use strict';
-
-    console.log("############################# SCRIPT LOADED ##########################################")
-    const excludeWebsites = ["www.cse-sncf-connect.com"]
+    let webSiteUrl = document.URL.split("/", 3)[2]
+    const excludeWebsites = []
 
     const wordings = ["Continuer sans accepter",
         "Continuer sans accepter →",
@@ -47,10 +33,10 @@
     var seekAndDestroyCookie = undefined
     var awaitPageLoad = undefined
 
-
-    //console.warn(document.URL.split("/", 3)[2])
-    if (!excludeWebsites.includes(document.URL.split("/", 3)[2])) {
+    if (!excludeWebsites.includes(webSiteUrl) && !getCookie(webSiteUrl)) {
         awaitPageLoad = window.setInterval(isPageLoaded, 20)
+    } else {
+        console.log("Script already executed or is disabled for this site")
     }
 
     function getSpan(text) {
@@ -83,6 +69,7 @@
 
     function seekAndClickRefuseCookie() {
         if (i >= maxTry) {
+            setCookie(webSiteUrl, true, 1)
             console.warn("Clear interval " + i)
             window.clearInterval(seekAndDestroyCookie);
         } else {
@@ -123,100 +110,28 @@
         }
     }
 
-})();
 
-    const wordings = ["Continuer sans accepter",
-        "Continuer sans accepter →",
-        "Refuser les cookies non nécessaires",
-        "Reject", "Tout rejeter",
-        "Tout Refuser",
-        "Refuser",
-        "Reject Optional Cookies",
-        "deny",
-        "Reject all",
-        "Use necessary cookies only",
-        "Only necessary cookies"
-    ]
-
-    const maxTry = 100
-    var i = 0
-    var seekAndDestroyCookie = undefined
-    var awaitPageLoad = undefined
-
-
-    //console.warn(document.URL.split("/", 3)[2])
-    if (!excludeWebsites.includes(document.URL.split("/", 3)[2])) {
-        awaitPageLoad = window.setInterval(isPageLoaded, 20)
+    function setCookie(cname, cvalue, exdays) {
+        const d = new Date();
+        d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+        let expires = "expires=" + d.toUTCString();
+        document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
     }
 
-    function getSpan(text) {
-        return Array.prototype.slice.call(document.querySelectorAll('span'))
-            .filter(function(el) {
-                return el.textContent.trim().toLowerCase() === text.toLowerCase()
-            })[0]
-    }
-
-    function getButton(text) {
-        return Array.prototype.slice.call(document.querySelectorAll('button'))
-            .filter(function(el) {
-                return el.textContent.trim().toLowerCase() === text.toLowerCase()
-            })[0]
-    }
-
-    function getLink(text) {
-        return Array.prototype.slice.call(document.querySelectorAll('a'))
-            .filter(function(el) {
-                return el.textContent.trim().toLowerCase() === text.toLowerCase()
-            })[0]
-    }
-
-    function isPageLoaded() {
-        if (document.readyState === "complete") {
-            seekAndDestroyCookie = window.setInterval(seekAndClickRefuseCookie, 150);
-            window.clearInterval(awaitPageLoad);
+    function getCookie(cname) {
+        let name = cname + "=";
+        let decodedCookie = decodeURIComponent(document.cookie);
+        let ca = decodedCookie.split(';');
+        for (let i = 0; i < ca.length; i++) {
+            let c = ca[i];
+            while (c.charAt(0) == ' ') {
+                c = c.substring(1);
+            }
+            if (c.indexOf(name) == 0) {
+                return c.substring(name.length, c.length);
+            }
         }
-    }
-
-    function seekAndClickRefuseCookie() {
-        if (i >= maxTry) {
-            console.warn("Clear interval " + i)
-            window.clearInterval(seekAndDestroyCookie);
-        } else {
-            wordings.some((element) => {
-                if (i >= maxTry) {
-                    return true;
-                }
-
-                var rbutton = getButton(element);
-                if (rbutton != undefined && i < maxTry) {
-                    console.warn("Cookies refused button")
-                    //console.info(element + " - " + i)
-                    rbutton.click()
-                    i = maxTry
-                    return true;
-                }
-
-                var rspan = getSpan(element);
-                if (rspan != undefined && i < maxTry) {
-                    console.warn("Cookies refused span")
-                    //console.log(element + " - " + i)
-                    rspan.click()
-                    i = maxTry
-                    return true;
-                }
-
-                var rlink = getLink(element);
-                if (rlink != undefined && i < maxTry) {
-                    console.warn("Cookies refused link")
-                    //console.log(element + " - " + i)
-                    rlink.click()
-                    i = maxTry
-                    return true;
-                }
-
-                i++
-            })
-        }
+        return false;
     }
 
 })();
